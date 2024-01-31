a38a74d531649ab362e2669bf89688a377193ef1
anaxios <skybosh.xyz@outlook.com>
2024-01-31 00:39:40 +0000

    clean up emoji replies and add filter to return only unique emoji

--- src/routes/api/emoji/index.ts
+++ src/routes/api/emoji/index.ts
@@ -1,95 +1,52 @@
-import { Ai } from "@cloudflare/ai";
-import type {
-  VectorizeIndex,
-  Fetcher,
-  Request,
-} from "@cloudflare/workers-types";
-
-import {
-  CloudflareVectorizeStore,
-  CloudflareWorkersAIEmbeddings,
-} from "@langchain/cloudflare";
-
-import { OpenAIEmbeddings } from "@langchain/openai";
-
-import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
+import type { VectorizeIndex, Fetcher } from "@cloudflare/workers-types";
 import { TogetherAI } from "@langchain/community/llms/togetherai";
-import { RetrievalQAChain } from "langchain/chains";
 import { PromptTemplate } from "langchain/prompts";
-
 import { Hono } from "hono";
-import { cors } from "hono/cors";
-import { bearerAuth } from "hono/bearer-auth";
-
-import { Pinecone } from "@pinecone-database/pinecone";
-import { PineconeStore } from "langchain/vectorstores/pinecone";
 import { JSONObject } from "hono/utils/types";
-
-export interface Env {
-  VECTORIZE_INDEX: VectorizeIndex;
-  AI: Fetcher;
-  TOKEN: string;
-}
-
-type AIResponse = {
-  vector?: JSONObject;
-  query?: JSONObject;
-};
-
-const token = "yourmom";
-const embed_model = "openai/text-embedding-ada-002";
 
 const app = new Hono();
 
+const Emoji = /\p{Extended_Pictographic}/u;
+
+function filterEmoji(t: string) {
+  const emojiArray = Array.from(t).filter((e) => Emoji.test(e));
+  return [...new Set(emojiArray)].join("");
+}
+
 app.get("/", async (c) => {
-  const query: string = c.req.query("query");
-  const temp: number = c.req.query("temp") || 0.5;
+  const query: string = c.req.query("query") || "";
+  const temp: number = parseFloat(c.req.query("temp") || "0.5");
   const llm: string =
     c.req.query("model") || "togethercomputer/llama-2-70b-chat";
 
   console.log(`query: ${query}`, `temp: ${temp} model: ${llm}`);
-  // const embeddings = new OpenAIEmbeddings({
-  //   openAIApiKey: c.env.OPENAI_API_KEY, // In Node.js defaults to process.env.OPENAI_API_KEY
-  //   batchSize: 512, // Default value if omitted is 512. Max is 2048
-  // });
-
-  // const store = new CloudflareVectorizeStore(embeddings, {
-  //   index: c.env.VECTORIZE_INDEX,
-  // });
 
   if (query) {
+    const apiKey = c.env?.TOGETHERAI_API_KEY as string;
+
+    if (!apiKey) {
+      // Handle the case where the API key is not provided
+      return c.json({ error: "API key is required" }, 400);
+    }
+
     const model = new TogetherAI({
-      apiKey: c.env.TOGETHERAI_API_KEY,
+      apiKey: apiKey,
       modelName: llm,
-      //modelName: "togethercomputer/llama-2-70b-chat",
-      temperature: parseFloat(temp),
+      temperature: temp,
       maxTokens: 128,
     });
 
     const prompt = PromptTemplate.fromTemplate(
       `Describe the following with only a few emoji: {question}`
     );
+
     const runnable = prompt.pipe(model);
+
     const response = await runnable.invoke({ question: query });
+
     console.log(response);
-    //   // RetrievalQAChain
-    //   const chain = RetrievalQAChain.fromLLM(model, store.asRetriever(), {
-    //     k: 2,
-    //     returnSourceDocuments: true,
-    //   });
 
-    //   const res = await chain.invoke({
-    //     query: query,
-    //   });
-
-    //   response.query = res.text;
-    // }
-
-    // if (vector === 'true') {
-    //   response.vector = await store.similaritySearch(query);
-    // }
-
-    return c.text(response);
+    return c.text(filterEmoji(response));
   }
 });
 
